00ded45ff924424efe709831b83967c5cfa09e5e
runup-dev <cs@run-up.co.kr>
2020-06-03 21:05:50 +0900

    메뉴얼 업데이트

--- update.sh
+++ update.sh
@@ -27,29 +27,30 @@ fi
 /usr/local/bin/wp cache flush
 /usr/local/bin/wp rewrite flush
 
+
+# 워드프레스 WP ROCKET을 사용중이라면 아래주석을 제거하세요 
 # ROCKET CACHE CLEAR
-
-echo "ROCKET CACHE CLEAR"
+#echo "ROCKET CACHE CLEAR"
 # 파일생성
-echo "<?php
-// 워드 프레스를로드합니다.
-require ( 'wp-load.php');
-
-// 캐시를 지 웁니다.
-if (function_exists ( 'rocket_clean_domain')) {
-        rocket_clean_domain ();
-}
-
-// 캐시를 미리로드합니다.
-if (function_exists ( 'run_rocket_sitemap_preload')) {
-        run_rocket_sitemap_preload ();
-}" > rocket-clean-domain.php
-
-SITE=$(/usr/local/bin/wp db query "SELECT option_value FROM $(/usr/local/bin/wp db prefix)options WHERE option_name='home'" --skip-column-names)
-ROCKET_CLEAR_URL=$SITE"/rocket-clean-domain.php"
-
+#echo "<?php
+#// 워드 프레스를로드합니다.
+#require ( 'wp-load.php');
+#
+#// 캐시를 지 웁니다.
+#if (function_exists ( 'rocket_clean_domain')) {
+#        rocket_clean_domain ();
+#}
+#
+#// 캐시를 미리로드합니다.
+#if (function_exists ( 'run_rocket_sitemap_preload')) {
+#        run_rocket_sitemap_preload ();
+#}" > rocket-clean-domain.php
+#
+#SITE=$(/usr/local/bin/wp db query "SELECT option_value FROM $(/usr/local/bin/wp db prefix)options WHERE option_name='home'" --skip-column-names)
+#ROCKET_CLEAR_URL=$SITE"/rocket-clean-domain.php"
+#
 #파일호출
-wget -q -O $ROCKET_CLEAR_URL &>/dev/null
+#wget -q -O $ROCKET_CLEAR_URL &>/dev/null
 
 #파일삭제
-rm rocket-clean-domain.php
+#rm rocket-clean-domain.php
